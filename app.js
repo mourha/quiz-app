b12da039f5c1429c07ec82e91ebe1963a9b1fdf4
Muhammad Murala <muhammadmurala360@gmail.com>
2024-09-30 10:49:38 +0100

    ch3

---
+++ app.js
@@ -0,0 +1,21 @@
+const startButton = document.getElementById('start-btn')
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+const nameFormContainer = document.getElementById('name-form-container')
+const submitNameButton = document.getElementById('submit-name-btn')
+const viewScoreButton = document.getElementById('view-score-btn')
+const nameInput = document.getElementById('name')
+
+// Event listener to capture the user's name and start the quiz
+submitNameButton.addEventListener('click', () => {
+    userName = nameInput.value
+    if (userName) {
+      nameFormContainer.classList.add('hide') // Hide name input form
+      startButton.classList.remove('hide') // Show start button after name is entered
+    } else {
+      alert("Please enter your name to start the quiz!") // Alert if no name is entered
+    }
+  })
+  
