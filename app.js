3eea6901f8788e59f3392ee5bdbdab2372f467af
Muhammad Murala <muhammadmurala360@gmail.com>
2024-09-30 11:05:19 +0100

    Function to set the correct/wrong class
    Function to set the correct/wrong class

--- app.js
+++ app.js
@@ -110,6 +110,21 @@ function saveScore() {
     }
     localStorage.setItem('quizUserData', JSON.stringify(userData)) // Save as JSON
   }
+  // Function to set the correct/wrong class
+function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+      element.classList.add('correct')
+    } else {
+      element.classList.add('wrong')
+    }
+  }
+  
+  // Function to clear the correct/wrong class
+  function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+  }
   
  
 
